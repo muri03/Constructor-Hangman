f8a4a39cb4dcb5b5c7466f27528b5b7eef275f37
Muriel <muriel.mangubat@gmail.com>
2017-11-27 16:37:51 -0600

    added a function but still incomplete

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var inquirer = require('inquirer');
 
-var wrongGuessesLeft = 0;
-var wins = 0;
-var losses = 0;
-var wordToGuess = "";
+// var wrongGuessesLeft = 0;
+// var wins = 0;
+// var losses = 0;
+// var wordToGuess = "";
 var teams = ["Liverpool", "Manchester City", "Chelsea", "Arsenal", "Manchester United"]
 var blanks = "";
-var lettersToGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var letterButtons = "";
-var correctGuessCount = 0;
-var wrongGuesses = 0;
+// var lettersToGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// var letterButtons = "";
+// var correctGuessCount = 0;
+// var wrongGuesses = 0;
 
 inquirer.prompt([{
     type: "confirm",
@@ -21,6 +21,7 @@ inquirer.prompt([{
     if (data.confirm) {
         // getTeams();
         console.log("What are the top English Premier League teams?");
+        getTeams();
 
     } else {
         console.log("Stick with MLS then");
@@ -30,17 +31,22 @@ inquirer.prompt([{
 
 function getTeams() {
 
-    // picks a random movie from the array
+    // picks a random team
     var randomTeams = teams[Math.floor(Math.random() * teams.length)];
 
-    // creates the blanks
-    blanks = "";
+    // creates the blanks;
     for (var i = 0; i < randomTeams.length; i++) {
         // Create an identifiable blank/letter
         var newBlank = "_";
         // Add the blank to the code
         blanks += newBlank;
-        console.log(blanks[i]);
+        console.log(blanks);
+        guessTeam();
     }
 
 };
+
+function guessTeam(teams, guess) {
+  this.teams = teams;
+  this.guess = guess;
+}
